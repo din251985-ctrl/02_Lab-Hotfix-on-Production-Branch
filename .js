f3b7e544d4b173615f9dc34c081178647466752f
din251985-ctrl <din251985@gmail.com>
2025-09-13 16:25:51 +0530

    Create .js

---
+++ .js
@@ -0,0 +1,7 @@
+function changeGreeting() {
+  const greeting = document.getElementById("greeting");
+  const message = document.getElementById("message");
+
+  greeting.textContent = "Namaste, Dinesh!";
+  message.textContent = "Thanks for visiting. This page is powered by HTML, CSS, and JavaScript.";
+}
